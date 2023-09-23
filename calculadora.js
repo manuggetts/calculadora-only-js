506b776871bf5a721cd6e7ac53384733d8038a61
manuggetts <manuggets@hotmail.com>
2023-09-22 21:17:09 -0300

    calc

--- calculadora.js
+++ calculadora.js
@@ -1,27 +1,65 @@
 function calculadora() {
     const operacao = prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração(-) \n 3 - Multiplicação(*) \n 4 - Divisão real(/)\n 5 - Divisão inteira(%) \n 6 - Potenciação(**)');
 
-    let n1 = Number(prompt('Insira o primeiro valor:'));
-    let n2 = Number(prompt('Insira o segundo valor:'));
-    let resultado;
+    if (!operacao || isNaN(operacao) || operacao < 1 || operacao > 6) {
+        alert('Erro - operação inválida!');
+    } else {
 
-    function soma() {
-        resultado = n1 + n2;
-        alert(`${n1} + ${n2} =${resultado}`)
+        let n1 = Number(prompt('Insira o primeiro valor:'));
+        let n2 = Number(prompt('Insira o segundo valor:'));
+        let resultado;
+
+        if (!isNaN(n1) && !isNaN(n2)) {
+            switch (operacao) {
+                case '1':
+                    resultado = n1 + n2;
+                    alert(`${n1} + ${n2} = ${resultado}`);
+                    break;
+                case '2':
+                    resultado = n1 - n2;
+                    alert(`${n1} - ${n2} = ${resultado}`);
+                    break;
+                case '3':
+                    resultado = n1 * n2;
+                    alert(`${n1} * ${n2} = ${resultado}`);
+                    break;
+                case '4':
+                    if (n2 !== 0) {
+                        resultado = n1 / n2;
+                        alert(`${n1} / ${n2} = ${resultado}`);
+                    } else {
+                        alert('Erro - divisão por zero não é permitida!');
+                    }
+                    break;
+                case '5':
+                    if (n2 !== 0) {
+                        resultado = n1 % n2;
+                        alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
+                    } else {
+                        alert('Erro - divisão por zero não é permitida!');
+                    }
+                    break;
+                case '6':
+                    resultado = n1 ** n2;
+                    alert(`${n1} elevado a ${n2} = ${resultado}`);
+                    break;
+                default:
+                    alert('Operação inválida');
+            }
+        } else {
+            alert('Erro - parâmetros inválidos!');
+        }
     }
 
-    if (operacao == 1) {
-        soma();
-    } else if (operacao == 2) {
-        subtracao();
-    } else if (operacao == 3) {
-        multiplicacao();
-    } else if (operacao == 4) {
-        divisaoReal();
-    } else if (operacao == 5) {
-        divisaoInteira();
-    } else if (operacao == 6) {
-        potenciacao();
+    let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+
+    if (opcao == '1') {
+        calculadora();
+    } else if (opcao == '2') {
+        alert('Até mais!')
+    } else {
+        alert('Digite uma opção válida!')
     }
 }
+
 calculadora();
